97a6a4d6e28d059fed39859b9c46fc0e8e5db2c5
Bodo Kaiser <i@bodokaiser.io>
2017-03-16 16:21:14 +0100

    updated summary and main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import numpy as np
 import os
 import torch
 import torch.nn
+import torch.multiprocessing as mp
 
-from torch.optim import Adam, SGD
+from torch.optim import Adam
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, ToTensor
@@ -23,8 +24,46 @@ def threshold(images):
 
     return Variable(images.gt(value).float())
 
+def train_epoch(args, epoch, model, writer, loader, optimizer):
+    model.train()
+
+    total_loss = 0
+
+    for step, (mr, us) in enumerate(loader):
+        if us.sum() < 10:
+            continue
+
+        mask = threshold(us)
+        inputs = Variable(mr)
+        targets = Variable(us)
+        outputs = model(inputs)
+
+        optimizer.zero_grad()
+        loss = outputs.mul(mask).dist(targets.mul(mask), 2)
+        loss.div_(mask.sum().data[0])
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.data[0]
+
+        if args.save_loss:
+            writer.write(epoch=epoch, step=step, loss=loss.data[0])
+
+    if args.save_images:
+        io.imsave(os.path.join(args.outdir, f'{epoch:03d}_input.png'),
+            inputs[0][0])
+        io.imsave(os.path.join(args.outdir, f'{epoch:03d}_output.png'),
+            outputs[0][0])
+        io.imsave(os.path.join(args.outdir, f'{epoch:03d}_target.png'),
+            targets[0][0])
+
+    print(f'training (epoch: {epoch}, loss: {total_loss}, pid: {pid})')
+
 def main(args):
     model = UNet()
+
+    if args.save_loss or args.save_images:
+        os.makedirs(args.outdir, exist_ok=True)
 
     loader = DataLoader(Concat([
         MnibiteNative(
@@ -40,44 +79,14 @@ def main(args):
                 ExpandDim(2),
                 ToTensor(),
             ]))) for i in args.train]),
-        batch_size=args.batch_size, num_workers=4, shuffle=True)
+        batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True)
 
-    writer = SummaryWriter(os.path.join(args.outdir, 'loss.json'))
-
-    optimizer = SGD(model.parameters(), lr=0.00001)
+    writer = SummaryWriter(args.outdir, 'loss.json')
+    
+    optimizer = Adam(model.parameters())
 
     for epoch in range(1, args.epochs+1):
-        train_loss = 0
-
-        for step, (mr, us) in enumerate(loader):
-            mask = threshold(us)
-
-            inputs = Variable(mr)
-            targets = Variable(us)
-            outputs = model(inputs)
-
-            optimizer.zero_grad()
-            loss = outputs.mul(mask).dist(targets.mul(mask), 2)
-            loss.div_(mask.sum().data[0])
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss.data[0]
-
-            if args.save_loss:
-                writer.write(epoch=epoch, step=step, loss=loss.data[0])
-
-        if args.save_images:
-            io.imsave(os.path.join(args.outdir, f'{epoch:03d}_input.png'),
-                inputs[0][0])
-            io.imsave(os.path.join(args.outdir, f'{epoch:03d}_output.png'),
-                outputs[0][0])
-            io.imsave(os.path.join(args.outdir, f'{epoch:03d}_target.png'),
-                targets[0][0])
-
-        print(f'training (epoch: {epoch}, loss: {train_loss}')
-
-    input('press enter to exit')
+        train_epoch(args, epoch, model, writer, loader, optimizer)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -86,7 +95,8 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', type=int, nargs='?', default=20)
     parser.add_argument('--outdir', type=str, nargs='?', default='output')
     parser.add_argument('--datadir', type=str, nargs='?', default='mnibite')
-    parser.add_argument('--batch_size', type=int, nargs='?', default=8)
+    parser.add_argument('--batch_size', type=int, nargs='?', default=1)
+    parser.add_argument('--num_workers', type=int, nargs='?', default=2)
     parser.add_argument('--save-loss', dest='save_loss', action='store_true')
     parser.add_argument('--save-images', dest='save_images', action='store_true')
     parser.set_defaults(show_loss=False, show_images=False)
--- mrtous/summary.py
+++ mrtous/summary.py
@@ -3,21 +3,25 @@ import os
 
 class SummaryWriter(object):
 
-    def __init__(self, filename):
-        dirname = os.path.dirname(filename)
-
-        if os.path.exists(dirname):
-            if os.path.exists(filename):
-                os.remove(filename)
-        else:
-            os.makedirs(dirname)
-
-        self.file = open(filename, 'w')
+    def __init__(self, dirname, filename):
         self.format = os.path.splitext(filename)[1]
+        assert self.format == '.json', 'SummaryWriter only supports JSON'
+
+        self.file = None
+        self.dirname = dirname
+        self.filename = filename
+
+    def open(self):
+        self.path = os.path.join(self.dirname, self.filename)
+
+        if os.path.exists(self.path):
+            os.remove(self.path)
+
+        self.file = open(self.path, 'w')
 
     def write(self, **event):
-        if self.format != '.json':
-            raise ValueError('SummaryWriter only support JSON')
+        if self.file is None:
+            self.open()
         self.file.write(json.dumps(event))
         self.file.write('\n')
         self.file.flush()
