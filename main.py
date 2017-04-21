511fe867b181ec80b636c7dffd97fa4ebfe5954d
Bodo Kaiser <i@bodokaiser.io>
2017-04-21 12:06:06 +0200

    added Clip, HistNormalize transform, fixed visualization

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+import numpy as np
+
 from argparse import ArgumentParser
 
 from torch.optim import SGD
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
-from torchvision.transforms import Compose, Normalize
+from torchvision.transforms import Compose, Normalize, Lambda
 
 from visdom import Visdom
 
 from mrtous.network import Simple
 from mrtous.dataset import MNIBITE
-from mrtous.transform import ToTensor
+from mrtous.transform import ToTensor, Clip, HistNormalize
 
 input_transform = Compose([
+    Clip(-25000, -20000),
+    HistNormalize(),
     ToTensor(),
-    Normalize([-30000], [2e16])
 ])
 target_transform = Compose([
+    HistNormalize(),
     ToTensor(),
-    Normalize([0], [2e8])
 ])
 
 def main(args):
@@ -25,16 +28,19 @@ def main(args):
     model.train()
 
     loader = DataLoader(MNIBITE(args.datadir, input_transform, target_transform),
-        num_workers=args.num_workers, batch_size=args.batch_size)
+        num_workers=args.num_workers)
     optimizer = SGD(model.parameters(), 1e-4)
 
     if args.vis_steps > 0:
-        vis = Visdom(args.vis_port)
+        vis = Visdom(port=args.vis_port)
 
     for epoch in range(1, args.num_epochs+1):
         epoch_loss = []
 
         for step, (mr, us) in enumerate(loader):
+            if us.sum() < 1e4:
+                continue
+
             inputs = Variable(mr)
             targets = Variable(us)
             outputs = model(inputs)
@@ -48,9 +54,9 @@ def main(args):
 
             if step % args.vis_steps == 0:
                 title = f'(epoch: {epoch}, step: {step})'
-                vis.heatmap(mr[0][0].numpy(), opts=dict(title=f'input {title}'))
-                #vis.image(us[0][0].byte().numpy(), opts=dict(title=f'target {title}'))
-                #vis.image(outputs[0][0].data.numpy(), opts=dict(title=f'output {title}'))
+                vis.image(mr[0][0], opts=dict(title=f'input {title}'))
+                vis.image(us[0][0], opts=dict(title=f'target {title}'))
+                vis.image(outputs[0][0].data, opts=dict(title=f'output {title}'))
 
         print(f'epoch: {epoch}, loss: {sum(epoch_loss)/len(epoch_loss)}')
 
@@ -65,7 +71,6 @@ if __name__ == '__main__':
     parser_train.add_argument('--datadir', required=True)
     parser_train.add_argument('--num-epochs', type=int, default=32)
     parser_train.add_argument('--num-workers', type=int, default=4)
-    parser_train.add_argument('--batch-size', type=int, default=32)
     parser_train.add_argument('--vis-port', type=int, default=3000)
     parser_train.add_argument('--vis-steps', type=int, default=0)
 
--- mrtous/transform.py
+++ mrtous/transform.py
@@ -1,6 +1,34 @@
 import torch
+import numpy as np
+
+from skimage.exposure import equalize_hist
+from skimage.filters import threshold_li
+
+class Clip:
+
+    def __init__(self, vmin, vmax):
+        self.vmin = vmin
+        self.vmax = vmax
+
+    def __call__(self, x):
+        return np.clip(x, self.vmin, self.vmax) - self.vmin
+
 
 class ToTensor:
 
     def __call__(self, x):
-        return torch.from_numpy(x.astype('float32')).unsqueeze(0)
+        return torch.from_numpy(x.astype(np.float32)).unsqueeze(0)
+
+
+class HistNormalize:
+
+    def __init__(self, nbins=256, mask=True):
+        self.mask = mask
+        self.nbins = nbins
+
+    def __call__(self, x):
+        try:
+            threshold = min(0, threshold_li(x))
+        except ValueError:
+            return np.zeros_like(x)
+        return equalize_hist(x, self.nbins, x > threshold)
