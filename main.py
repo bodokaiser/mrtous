8a61b200ff45a40eabcef2e5ed1beb994145a996
Bodo Kaiser <i@bodokaiser.io>
2017-04-22 11:59:48 +0200

    added support for state saving, loading

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import time
+import torch
 
 from argparse import ArgumentParser
 
@@ -36,6 +38,8 @@ def main(args):
 
     if args.cuda:
         model = model.cuda()
+    if args.state:
+        model.load_state_dict(torch.load(args.state))
 
     loader = DataLoader(MNIBITE(args.datadir, input_transform, target_transform))
     optimizer = SGD(model.parameters(), 1e-4)
@@ -67,20 +71,22 @@ def main(args):
 
             epoch_loss.append(loss.data[0])
 
-            if step % args.vis_steps == 0:
+            if args.log_steps > 0 and step & args.log_steps == 0:
+                print(f'epoch: {epoch}, step: {step}, loss: {sum(epoch_loss)/len(epoch_loss)}')
+            if args.vis_steps > 0 and step % args.vis_steps == 0:
                 title = f'(epoch: {epoch}, step: {step})'
                 vis.image(inputs[0][0].cpu().data, opts=dict(title=f'input {title}'))
                 vis.image(outputs[0][0].cpu().data, opts=dict(title=f'output {title}'))
                 vis.image(targets[0][0].cpu().data, opts=dict(title=f'target {title}'))
-            if step & args.log_steps == 0:
-                print(f'epoch: {epoch}, step: {step}, loss: {sum(epoch_loss)/len(epoch_loss)}')
-
-        print(f'epoch: {epoch}, loss: {sum(epoch_loss)/len(epoch_loss)}')
+            if args.save_steps > 0 and step % args.save_steps == 0:
+                stamp = time.strftime("%Y%m%d-%H%M%S")
+                torch.save(model.state_dict(), f'{args.model}-{stamp}.pth')
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--cuda', action='store_true')
     parser.add_argument('--model', choices=['one', 'two', 'unet'], required=True)
+    parser.add_argument('--state')
 
     subparsers = parser.add_subparsers(dest='action')
     subparsers.required = True
@@ -91,5 +97,6 @@ if __name__ == '__main__':
     parser_train.add_argument('--vis-port', type=int, default=3000)
     parser_train.add_argument('--vis-steps', type=int, default=0)
     parser_train.add_argument('--log-steps', type=int, default=0)
+    parser_train.add_argument('--save-steps', type=int, default=0)
 
     main(parser.parse_args())
